6c1df9d3cdc506e9d1f82cac7703607cb54b5b6e
Y2theK <yeyintkyaw.dev@gmail.com>
2025-03-17 22:42:34 +0630

    feat: delete old file

--- src/controllers/user/profileController.ts
+++ src/controllers/user/profileController.ts
@@ -6,6 +6,8 @@ import { createError } from "../../util/error";
 import { getUserById, updateUser } from "../../services/authService";
 import { checkUserIfNotExist } from "../../util/auth";
 import { checkFileIfNotExist } from "../../util/check";
+import { unlink } from "fs";
+import path from "path";
 
 interface CustomRequest extends Request {
   userId?: number;
@@ -39,13 +41,33 @@ export const uploadProfile = async (
 ) => {
   const userId = req.userId;
   const image = req.file;
-  const user = await getUserById(userId!);
+  const user: any = await getUserById(userId!);
   checkUserIfNotExist(user);
   checkFileIfNotExist(image);
 
-  await updateUser(userId!, { image: image!.path });
+  const fileName = image?.filename;
+
+  // delete old image
+  if (user.image) {
+    // we have to use path.join to get the correct path
+    const filePath = path.join(
+      __dirname,
+      "../../../",
+      "upload/images/",
+      user.image // in window system we get upload\\image.jpg so we need to replace it to upload/image.jpg
+    );
+    await unlink(filePath, (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
+
+  // update new image
+  await updateUser(userId!, { image: fileName });
 
   res.status(200).json({
     message: "Profile Picture Uploaded",
+    image: fileName,
   });
 };
