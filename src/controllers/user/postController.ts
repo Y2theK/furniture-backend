0c4556478bc018952c3c485e64cbed01f6194147
Y2theK <yeyintkyaw.dev@gmail.com>
2025-03-25 23:33:23 +0630

    feat: modified posts response

--- src/controllers/user/postController.ts
+++ src/controllers/user/postController.ts
@@ -1,4 +1,4 @@
-import { param } from "express-validator";
+import { body, param } from "express-validator";
 import { getPostById, getPostWithRelation } from "../../services/postService";
 import { Request, Response } from "express";
 import { getUserById } from "../../services/authService";
@@ -19,9 +19,30 @@ export const getPost = [
     const post = await getPostWithRelation(+postId); //+postId is the same as parseInt(postId)
     checkModelIfNotExist(post);
 
+    const modifiedPost = {
+      id: post?.id,
+      title: post?.title,
+      content: post?.content,
+      body: post?.body,
+      image: "/optimize/" + post?.image.split(".")[0] + ".webp",
+      fullName:
+        post?.author.firstName ?? "" + " " + post?.author.lastName ?? "",
+      category: post?.category.name,
+      type: post?.type.name,
+      tags:
+        post?.tags && post?.tags.length > 0
+          ? post?.tags.map((tag) => tag.name)
+          : null,
+      updatedAt: post?.updatedAt.toLocaleDateString("en-US", {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+      }), // format date - "March 23, 2025"
+    };
+
     res.status(200).json({
       message: "Post fetched successfully",
-      post,
+      post: modifiedPost,
     });
   },
 ];
